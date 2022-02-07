0a4413c52877341e7f5bdd2e0332e8e2e49a643c
Hans Ott <hansott@hotmail.be>
2022-02-07 12:04:53 +0100

    ✨ Add account ID and verification to page views

--- lib/index.ts
+++ lib/index.ts
@@ -6,6 +6,11 @@ declare namespace JournyIO_ {
   type Metadata = Record<string, string | boolean | number | Date>;
 
   type InitArgs = { trackerId: string; domain: string };
+  
+  type PageArgs = {
+    accountId?: string;
+    verification?: string;
+  };
 
   type IdentifyArgs = {
     email?: string;
@@ -28,8 +33,8 @@ declare namespace JournyIO_ {
   };
 
   interface JournyIO {
-    (command: "init", args: InitArgs): void;
-    (command: "pageview"): void;
+    (command: "init", args: InitArgs, callback?: Callback): void;
+    (command: "pageview", args?: PageArgs, callback?: Callback): void;
     (command: "identify", args: IdentifyArgs, callback?: Callback): void;
     (command: "account", args: AccountArgs, callback?: Callback): void;
     (command: "event", args: EventArgs, callback?: Callback): void;
